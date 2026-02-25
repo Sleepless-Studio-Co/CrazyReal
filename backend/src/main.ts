580eefd85f9f6f063196cd7536512969d0e13569
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-02-25 16:58:46 +0100

    refactor: clean up comments in app.controller.ts and main.ts for clarity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  // On précise qu'on utilise Express explicitement
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // LA LIGNE MAGIQUE : On rend le dossier /uploads accessible publiquement
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
