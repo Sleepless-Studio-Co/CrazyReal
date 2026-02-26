20707fd01a9a6d6f71497a05e76316cd33a4f3f8
titouan.bouillot-bachelier <titouan.bouillot-bachelier@epitech.eu>
2026-02-26 16:19:07 +0100

    feat: create UsersModule for user management integration

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService], // Exporte pour être utilisé dans auth module
+})
+export class UsersModule {}
