0f01fa3233874e8a068dc45a42e205f792a8b3a6
EvannBloutin <evann.bloutin@epitech.eu>
2026-07-08 19:08:55 +0200

    fix: rm largeIcon

--- mobile/lib/services/notification_service.dart
+++ mobile/lib/services/notification_service.dart
@@ -99,7 +99,6 @@ class NotificationService {
       importance: Importance.max,
       priority: Priority.high,
       icon: 'ic_notification',
-      largeIcon: const DrawableResourceAndroidBitmap('logo_app'),
       color: const Color(0xFF3B2A21),
     );
 
